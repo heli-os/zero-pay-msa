3017a3f851b27e142021a8c615ce8ee4f68089c7
Heli <sun@jupiterflow.com>
2022-07-12 08:18:29 +0900

    feat: add finance van module

---
+++ finance-van/src/main/java/kr/co/zerobase/financevan/FinanceVanApplication.java
@@ -0,0 +1,13 @@
+package kr.co.zerobase.financevan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceVanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceVanApplication.class, args);
+    }
+
+}
